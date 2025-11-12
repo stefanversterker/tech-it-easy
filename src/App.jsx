710571759cb1c71adbdf2aa8755eb41f21218dbb
stefanversterker <stefanversterker@gmail.com>
2025-11-12 12:44:50 +0100

    html en css basis staat

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,34 @@ import './App.css';
 
 function App() {
     return (
-        <div>
-            <header>
-                <h1>Tech it easy dashboard</h1>
-            </header>
-            <main>
-                <section>
-                    <title>
+            <div className="everything">
+                <header>
+                    <h1>Tech it easy dashboard</h1>
+                </header>
+                <main className="all-tiles">
+                    <div>
                         <h2>Verkoopoverzicht</h2>
-                    </title>
-                    <div>
-                        <article className="info-tile"></article>
-                        <article></article>
-                        <article></article>
                     </div>
-                </section>
-                <section>
-                    <title>
+                    <section>
+
+                        <div className="info-tile-container">
+                            <article className="info-tile green-tile"></article>
+                            <article className="info-tile blue-tile"></article>
+                            <article className="info-tile red-tile"></article>
+                        </div>
+                    </section>
+                    <div>
                         <h2>Best verkochte tv</h2>
-                    </title>
-                    <div>
-                        <picture></picture>
-                        <article></article>
                     </div>
-                </section>
-            </main>
-        </div>
+                    <section>
+
+                        <div className="top-seller">
+                            <picture></picture>
+                            <article></article>
+                        </div>
+                    </section>
+                </main>
+            </div>
     )
 }
 
