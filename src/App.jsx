59cf02ed08941ebba5dde0c4c49d4793b0af686c
stefanversterker <stefanversterker@gmail.com>
2025-11-12 11:24:17 +0100

    eerste commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import './App.css';
 
 function App() {
-  return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
-  )
+    return (
+        <div>
+            <header>
+                <h1>Tech it easy dashboard</h1>
+            </header>
+
+            <main>
+                <section>
+                    <title>
+                        <h2>Verkoopoverzicht</h2>
+                    </title>
+                    <div>
+                        <article></article>
+                        <article></article>
+                        <article></article>
+                    </div>
+                </section>
+
+                <section>
+                    <title>
+                        <h2>Best verkochte tv</h2>
+                    </title>
+                    <div>
+                        <picture></picture>
+                        <article></article>
+                    </div>
+                </section>
+            </main>
+        </div>
+    )
 }
 
 export default App
