a8eecbc68cff90d7bee67cd64ede5a9b6e705f63
stefanversterker <stefanversterker@gmail.com>
2025-11-12 13:35:38 +0100

    tegels grotendeels statisch ingevuld

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,71 @@ import './App.css';
 
 function App() {
     return (
-            <div className="everything">
-                <header>
-                    <h1>Tech it easy dashboard</h1>
-                </header>
-                <main className="all-tiles">
-                    <div>
-                        <h2>Verkoopoverzicht</h2>
-                    </div>
-                    <section>
+        <div className="everything">
+            <header>
+                <h1>Tech it easy dashboard</h1>
+            </header>
+            <main className="all-tiles">
+                <div>
+                    <h2>Verkoopoverzicht</h2>
+                </div>
+                <section>
 
-                        <div className="info-tile-container">
-                            <article className="info-tile green-tile"></article>
-                            <article className="info-tile blue-tile"></article>
-                            <article className="info-tile red-tile"></article>
-                        </div>
-                    </section>
-                    <div>
-                        <h2>Best verkochte tv</h2>
+                    <div className="info-tile-container">
+                        <article className="info-tile green-tile">
+                            <div>
+                                <h2>Aantal verkochte producten</h2>
+                            </div>
+                            <div>
+                                <h2 id="products-sold">33</h2>
+                            </div>
+                        </article>
+                        <article className="info-tile blue-tile">
+                            <div>
+                                <h2>Aantal ingekochte producten</h2>
+                            </div>
+                            <div>
+                                <h2 id="products-purchased">73</h2>
+                            </div>
+                        </article>
+                        <article className="info-tile red-tile">
+                            <div>
+                                <h2>Aantal te verkopen producten</h2>
+                            </div>
+                            <div>
+                                <h2 id="products-stock">40</h2>
+                            </div>
+                        </article>
                     </div>
-                    <section>
+                </section>
+                <div>
+                    <h2>Best verkochte tv</h2>
+                </div>
+                <section>
 
-                        <div className="top-seller">
-                            <picture></picture>
-                            <article></article>
-                        </div>
-                    </section>
-                </main>
-            </div>
+                    <div className="top-seller">
+                        <div className="top-seller-image"></div>
+                        <article>
+                            <div>
+                                <div id="product-title">DungBox2000</div>
+                                <div id="product-description">TeeVee</div>
+                            </div>
+                            <div id="product-price">1000</div>
+                            <div id="product-available-sizes">43 inch | 50 inch | 55 inch | 58 inch</div>
+                            <div id="product-specs">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+            </main>
+        </div>
     )
 }
 
